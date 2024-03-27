95a408a336749101fad60c2fce59a31a590c2ba0
nbalcarc <nathan.balcarcel@wsu.edu>
2024-03-27 10:20:34 +0000

    Finished first version of monte carlo

--- application/src/main.rs
+++ application/src/main.rs
@@ -4,9 +4,7 @@ use common::{monte, regular};
 use rand;
 
 
-fn main() {
-    println!("Hello, world!");
-
+fn short() {
     let a = [
         3.0, 2.0, 1.0, 5.0,
         9.0, 1.0, 3.0, 0.0,
@@ -19,13 +17,23 @@ fn main() {
     ];
     let sizes = (2, 4, 3);
     println!("{:?}", regular::multiply_float(&a, &b, sizes));
+    println!("{:?}", monte::multiply_float(&a, &b, sizes, 4));
+}
 
+fn long() {
 
     let sizes = (2_048, 1_536, 1_024);
     let a: Vec<f32> = (0..sizes.0*sizes.1).into_iter().map(|_| rand::random()).collect();
     let b: Vec<f32> = (0..sizes.1*sizes.2).into_iter().map(|_| rand::random()).collect();
-    let c = monte::multiply_float(&a, &b, sizes);
+    let c = monte::multiply_float(&a, &b, sizes, 500);
     println!("{:?}, {:?}", c, c.iter().sum::<f32>());
+}
+
+
+fn main() {
+    println!("Hello, world!");
+    short();
+
 }
 
 
--- common/src/monte.rs
+++ common/src/monte.rs
@@ -1,46 +1,102 @@
-use rand::thread_rng;
+use rand::{seq::SliceRandom, thread_rng};
+
+use crate::regular;
 
 
 
 /// Downscale through random sampling
-pub fn multiply_float(a: &[f32], b: &[f32], sizes: (usize, usize, usize), s_size: u32) -> Vec<f32> {
+pub fn multiply_float(a: &[f32], b: &[f32], sizes: (usize, usize, usize), downsample: usize) -> Vec<f32> {
     // note: sizes = (A height, A width && B height, B width)
-    let mut c = vec![0.0; sizes.0 * sizes.2];
-    let (m, n, p) = sizes;
-    //let bracket_a: f64 = a.iter().map(|n| (n * n) as f64).sum();
+    //let mut c = vec![0.0; sizes.0 * sizes.2];
+    //let (m, n, p) = sizes;
+
+    //let columns = Vec::with_capacity(sizes.1);
+    let mut a_columns: Vec<Vec<f32>> = (0..sizes.1).into_iter() //columns of A
+        .map(|_| Vec::with_capacity(sizes.0))
+        .collect();
+    for i in 0..a.len() {
+        a_columns[i % sizes.1].push(a[i]);
+    }
+
+
+    //todo!();
 
     // TODO: CONSIDER USING AN ITERATOR WITH CHUNKING HERE INSTEAD OF A LOOP
-    println!("{}, {}, {}", a.len(), sizes.1, a.len() / sizes.0);
+    //println!("{}, {}, {}", a.len(), sizes.1, a.len() / sizes.0);
     let mut column_squared_magnitudes = vec![0.0; sizes.1]; //used to calculate magnitude of each column
     for i in 0..a.len() { //iterate through all values in A
-        //let squared = a[i] as f64 * a[i] as f64;
-        //bracket_sum += squared;
         column_squared_magnitudes[i / sizes.0] += a[i] as f64 * a[i] as f64;
     }
 
     let bracket_a: f64 = column_squared_magnitudes.iter().sum();
-    //let column_magnitudes: Vec<f64> = column_squares_sum.iter().map(|x| x.sqrt()).collect();
-
-    //let mut probs = vec![0.0; sizes.1]; //weight of each column
     let probs: Vec<f64> = column_squared_magnitudes.iter().map(|x| x / bracket_a).collect();
-    println!("{:?}, {:?}", probs, probs.iter().sum::<f64>());
+    //println!("{:?}, {:?}", probs, probs.iter().sum::<f64>());
 
     let columns: Vec<usize> = (0..sizes.1).collect();
 
+    //columns.choose_multiple_weighted(&mut thread_rng(), amount, weight);
+    let mut rng = thread_rng();
+    //zip(columns, probs);
+    let zipped: Vec<((usize, f64), f64)> = columns.iter()
+        .zip(probs.iter())
+        .map(|x| ((*x.0, *x.1), *x.1))
+        .collect();
+    let chosen: Vec<(usize, f64)> = (0..downsample).into_iter()
+        .map(|_| zipped.choose_weighted(&mut rng, |x| x.1).unwrap().0)
+        .collect();
 
+    //println!("{:?}", chosen);
 
-    for i in 0..m { //A height
-        for j in 0..p { //B width
-            // note: chooses a row in A and a column in B and sums the products
-            let mut sum = 0.0;
-            for k in 0..n { //A width and B height
-                sum += a[i*n + k] * b[k*p + j];
-            }
-            c[i*p + j] = sum;
-        }
+    //let mut s = vec![0.0; sizes.0 * downsample]; //new A
+    //let mut r = vec![0.0; sizes.2 * downsample]; //new B
+    let mut s = Vec::with_capacity(sizes.0 * downsample); //new A
+    let mut r = Vec::with_capacity(sizes.2 * downsample); //new B
+
+    for choice in chosen {
+        let index = choice.0;
+        let prob = choice.1;
+        //s.extend_from_slice(&a[(choice..).step_by(sizes.1)]);
+        //let indices: Vec<usize> = (choice..a.len()).step_by(sizes.1).collect();
+        //for index in indices {
+        //    s.push(a[index]);
+        //}
+        //r.extend_from_slice(&a[(choice..).step_by(sizes.1)]);
+
+        //s.extend_from_slice(&a_columns[choice]);
+        //r.extend_from_slice(&b[sizes.2 * choice..sizes.2 * (choice+1)]);
+        let adjusted_scol: Vec<f32> = a_columns[index].iter()
+            .map(|x| x / (downsample as f32 * prob as f32).sqrt())
+            .collect();
+        let adjusted_rcol: Vec<f32> = b[sizes.2 * index..sizes.2 * (index+1)].iter()
+            .map(|x| x / (downsample as f32 * prob as f32).sqrt())
+            .collect();
+        s.extend(adjusted_scol);
+        r.extend(adjusted_rcol);
     }
 
+    let c = regular::multiply_float(&s, &r, (sizes.0, downsample, sizes.2));
+
     c
+
+    //todo!();
+    
+
+    //regular::multiply_float(a, b, sizes)
+
+
+
+    //for i in 0..m { //A height
+    //    for j in 0..p { //B width
+    //        // note: chooses a row in A and a column in B and sums the products
+    //        let mut sum = 0.0;
+    //        for k in 0..n { //A width and B height
+    //            sum += a[i*n + k] * b[k*p + j];
+    //        }
+    //        c[i*p + j] = sum;
+    //    }
+    //}
+
+    //c
 }
 
 
