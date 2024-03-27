aebd4c8b398a5031278491d49abcc76d5ce8824b
nbalcarc <nathan.balcarcel@wsu.edu>
2024-03-27 14:00:40 -0700

    a

--- common/benches/my_benchmark.rs
+++ common/benches/my_benchmark.rs
@@ -3,19 +3,26 @@ use std::time::Duration;
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand;
 
-use common::regular;
+use common::{monte, regular};
 
 
 fn benchmarks(c: &mut Criterion) {
     let sizes = (2_048, 1_536, 1_024);
 
-    let a: Vec<f32> = (0..sizes.0*sizes.1).into_iter().map(|_| rand::random()).collect();
-    let b: Vec<f32> = (0..sizes.1*sizes.2).into_iter().map(|_| rand::random()).collect();
-    c.bench_function("regular float", |x| x.iter(|| regular::multiply_float(&a, &b, sizes)));
-
-    let a: Vec<i32> = (0..sizes.0*sizes.1).into_iter().map(|_| rand::random()).collect();
-    let b: Vec<i32> = (0..sizes.1*sizes.2).into_iter().map(|_| rand::random()).collect();
-    c.bench_function("regular int", |x| x.iter(|| regular::multiply_int(&a, &b, sizes)));
+    let a_float: Vec<f32> = (0..sizes.0*sizes.1).into_iter().map(|_| rand::random()).collect();
+    let b_float: Vec<f32> = (0..sizes.1*sizes.2).into_iter().map(|_| rand::random()).collect();
+    let a_int: Vec<i32> = (0..sizes.0*sizes.1).into_iter().map(|_| rand::random()).collect();
+    let b_int: Vec<i32> = (0..sizes.1*sizes.2).into_iter().map(|_| rand::random()).collect();
+    c.bench_function("regular float", |x| x.iter(|| regular::multiply_float(&a_float, &b_float, sizes)));
+    c.bench_function("regular int", |x| x.iter(|| regular::multiply_int(&a_int, &b_int, sizes)));
+    c.bench_function("monte float 1152", |x| x.iter(|| monte::multiply_float(&a_float, &b_float, sizes, 1152)));
+    c.bench_function("monte int 1152", |x| x.iter(|| monte::multiply_int(&a_int, &b_int, sizes, 1152)));
+    c.bench_function("monte float 768", |x| x.iter(|| monte::multiply_float(&a_float, &b_float, sizes, 768)));
+    c.bench_function("monte int 768", |x| x.iter(|| monte::multiply_int(&a_int, &b_int, sizes, 768)));
+    c.bench_function("monte float 384", |x| x.iter(|| monte::multiply_float(&a_float, &b_float, sizes, 384)));
+    c.bench_function("monte int 384", |x| x.iter(|| monte::multiply_int(&a_int, &b_int, sizes, 384)));
+    c.bench_function("monte float 192", |x| x.iter(|| monte::multiply_float(&a_float, &b_float, sizes, 192)));
+    c.bench_function("monte int 192", |x| x.iter(|| monte::multiply_int(&a_int, &b_int, sizes, 192)));
 }
 
 criterion_group!{
--- common/src/monte.rs
+++ common/src/monte.rs
@@ -60,3 +60,61 @@ pub fn multiply_float(a: &[f32], b: &[f32], sizes: (usize, usize, usize), downsa
 }
 
 
+/// Downscale through random sampling
+pub fn multiply_int(a: &[i32], b: &[i32], sizes: (usize, usize, usize), downsample: usize) -> Vec<f32> {
+    // note: sizes = (A height, A width && B height, B width)
+
+    // turn A into columns for ease of access
+    let mut a_columns: Vec<Vec<i32>> = (0..sizes.1).into_iter() //columns of A
+        .map(|_| Vec::with_capacity(sizes.0))
+        .collect();
+    for i in 0..a.len() {
+        a_columns[i % sizes.1].push(a[i]);
+    }
+
+    // get column magnitudes
+    let column_squared_magnitudes: Vec<f64> = a_columns.iter()
+        .map(|col|
+             col.iter()
+                 .map(|x| (*x as f64) * (*x as f64))
+                 .sum()
+        )
+        .collect();
+
+    // generate probabilities for each column
+    let bracket_a: f64 = column_squared_magnitudes.iter().sum();
+    let probs: Vec<f64> = column_squared_magnitudes.iter().map(|x| x / bracket_a).collect();
+
+    // select the columns to use
+    let mut rng = thread_rng();
+    let columns: Vec<usize> = (0..sizes.1).collect();
+    let zipped: Vec<((usize, f64), f64)> = columns.iter()
+        .zip(probs.iter())
+        .map(|x| ((*x.0, *x.1), *x.1))
+        .collect();
+    let chosen: Vec<(usize, f64)> = (0..downsample).into_iter()
+        .map(|_| zipped.choose_weighted(&mut rng, |x| x.1).unwrap().0)
+        .collect();
+
+    // fix columns and push to S and R
+    let mut s = Vec::with_capacity(sizes.0 * downsample); //new A
+    let mut r = Vec::with_capacity(sizes.2 * downsample); //new B
+    for choice in chosen {
+        let index = choice.0;
+        let prob = choice.1;
+
+        let adjusted_scol: Vec<f32> = a_columns[index].iter()
+            .map(|x| *x as f32 / (downsample as f32 * prob as f32).sqrt())
+            .collect();
+        let adjusted_rcol: Vec<f32> = b[sizes.2 * index..sizes.2 * (index+1)].iter()
+            .map(|x| *x as f32 / (downsample as f32 * prob as f32).sqrt())
+            .collect();
+        s.extend(adjusted_scol);
+        r.extend(adjusted_rcol);
+    }
+
+    // return final result
+    regular::multiply_float(&s, &r, (sizes.0, downsample, sizes.2))
+}
+
+
