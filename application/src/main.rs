cf8f726ce25aed47776754a94af3814e1ebc6c5b
nbalcarc <nathan.balcarcel@wsu.edu>
2024-03-28 11:53:32 +0000

    Experimenting with compression alg

--- application/src/main.rs
+++ application/src/main.rs
@@ -1,5 +1,15 @@
-use common::{monte, regular};
+use std::time::Instant;
+
+use common::{monte, regular, tools::stats};
 use rand;
+
+
+#[derive(Debug, Clone)]
+struct PerfBlock {
+    name: String,
+    time: f64,
+    mae: f64,
+}    
 
 
 fn short() {
@@ -27,149 +37,170 @@ fn long() {
     println!("{:?}, {:?}", c, c.iter().sum::<f32>());
 }
 
+fn thing() {
+    let sizes = (2_048, 1_536, 1_024);
+    let trials = 1;
+
+    println!("Generating matrices");
+    let a: Vec<f32> = (0..sizes.0*sizes.1).into_iter().map(|_| rand::random()).collect();
+    let b: Vec<f32> = (0..sizes.1*sizes.2).into_iter().map(|_| rand::random()).collect();
+    //let a_int: Vec<i32> = (0..sizes.0*sizes.1).into_iter().map(|_| rand::random()).collect();
+    //let b_int: Vec<i32> = (0..sizes.1*sizes.2).into_iter().map(|_| rand::random()).collect();
+
+    let mut c = Vec::new();
+
+    let mut perf_blocks = Vec::with_capacity(16);
+
+    // regular
+    let test_name = String::from("regular");
+    println!("Running test: {}", test_name);
+    let mut times = Vec::with_capacity(trials);
+    for i in 0..trials {
+        println!("Iteration {}", i);
+        let now = Instant::now();
+        c = regular::multiply_float(&a, &b, sizes);
+        times.push(now.elapsed());
+    }
+    perf_blocks.push(PerfBlock {
+        name: test_name,
+        time: times.into_iter().map(|x| x.as_secs_f64()).sum::<f64>() / trials as f64,
+        mae: 0.0,
+    });
+
+    // regular int
+    //let test_name = String::from("regular int");
+    //println!("Running test: {}", test_name);
+    //let mut times = Vec::with_capacity(trials);
+    //for i in 0..trials {
+    //    println!("Iteration {}", i);
+    //    let now = Instant::now();
+    //    regular::multiply_int(&a_int, &b_int, sizes);
+    //    times.push(now.elapsed());
+    //}
+    //perf_blocks.push(PerfBlock {
+    //    name: test_name,
+    //    time: times.into_iter().map(|x| x.as_secs_f64()).sum::<f64>() / trials as f64,
+    //    mae: 0.0,
+    //});
+
+    // monte carlo 1152
+    let test_name = String::from("monte 1152");
+    println!("Running test: {}", test_name);
+    let mut times = Vec::with_capacity(trials);
+    let mut accuracies = Vec::with_capacity(trials);
+    for i in 0..trials {
+        println!("Iteration {}", i);
+        let now = Instant::now();
+        let c_prime = monte::multiply(&a, &b, sizes, 1152);
+        times.push(now.elapsed());
+        accuracies.push(stats(&c, &c_prime));
+    }
+    perf_blocks.push(PerfBlock {
+        name: test_name,
+        time: times.into_iter().map(|x| x.as_secs_f64()).sum::<f64>() / trials as f64,
+        mae: accuracies.into_iter().sum::<f64>() / trials as f64,
+    });
+
+    // monte carlo 768
+    let test_name = String::from("monte 768");
+    println!("Running test: {}", test_name);
+    let mut times = Vec::with_capacity(trials);
+    let mut accuracies = Vec::with_capacity(trials);
+    for i in 0..trials {
+        println!("Iteration {}", i);
+        let now = Instant::now();
+        let c_prime = monte::multiply(&a, &b, sizes, 768);
+        times.push(now.elapsed());
+        accuracies.push(stats(&c, &c_prime));
+    }
+    perf_blocks.push(PerfBlock {
+        name: test_name,
+        time: times.into_iter().map(|x| x.as_secs_f64()).sum::<f64>() / trials as f64,
+        mae: accuracies.into_iter().sum::<f64>() / trials as f64,
+    });
+
+    // monte carlo 384
+    let test_name = String::from("monte 384");
+    println!("Running test: {}", test_name);
+    let mut times = Vec::with_capacity(trials);
+    let mut accuracies = Vec::with_capacity(trials);
+    for i in 0..trials {
+        println!("Iteration {}", i);
+        let now = Instant::now();
+        let c_prime = monte::multiply(&a, &b, sizes, 384);
+        times.push(now.elapsed());
+        accuracies.push(stats(&c, &c_prime));
+    }
+    perf_blocks.push(PerfBlock {
+        name: test_name,
+        time: times.into_iter().map(|x| x.as_secs_f64()).sum::<f64>() / trials as f64,
+        mae: accuracies.into_iter().sum::<f64>() / trials as f64,
+    });
+
+    // monte carlo 192
+    let test_name = String::from("monte 192");
+    println!("Running test: {}", test_name);
+    let mut times = Vec::with_capacity(trials);
+    let mut accuracies = Vec::with_capacity(trials);
+    for i in 0..trials {
+        println!("Iteration {}", i);
+        let now = Instant::now();
+        let c_prime = monte::multiply(&a, &b, sizes, 192);
+        times.push(now.elapsed());
+        accuracies.push(stats(&c, &c_prime));
+    }
+    perf_blocks.push(PerfBlock {
+        name: test_name,
+        time: times.into_iter().map(|x| x.as_secs_f64()).sum::<f64>() / trials as f64,
+        mae: accuracies.into_iter().sum::<f64>() / trials as f64,
+    });
+
+
+
+    println!("{:?}", perf_blocks);
+
+
+
+
+}
+
 
 fn main() {
     println!("Hello, world!");
-    short();
+    //short();
+    thing();
 }
 
 
 
 
-#[cfg(test)]
-mod tests {
-    use common::tools::stats;
-
-    use super::*;
-    use std::time::Instant;
-
-
-    #[derive(Debug, Clone)]
-    struct PerfBlock {
-        name: String,
-        time: f64,
-        mae: f64,
-    }    
-
-
-    #[test]
-    fn benchmark() {
-        let sizes = (2_048, 1_536, 1_024);
-        let trials = 10;
-
-        let a: Vec<f32> = (0..sizes.0*sizes.1).into_iter().map(|_| rand::random()).collect();
-        let b: Vec<f32> = (0..sizes.1*sizes.2).into_iter().map(|_| rand::random()).collect();
-        let a_int: Vec<i32> = (0..sizes.0*sizes.1).into_iter().map(|_| rand::random()).collect();
-        let b_int: Vec<i32> = (0..sizes.1*sizes.2).into_iter().map(|_| rand::random()).collect();
-
-        let mut perf_blocks = Vec::with_capacity(16);
-
-        // regular
-        let test_name = String::from("regular");
-        let mut times = Vec::with_capacity(trials);
-        for _ in 0..trials {
-            let now = Instant::now();
-            regular::multiply_float(&a, &b, sizes);
-            times.push(now.elapsed());
-        }
-        perf_blocks.push(PerfBlock {
-            name: test_name,
-            time: times.into_iter().map(|x| x.as_secs_f64()).sum::<f64>() / trials as f64,
-            mae: 0.0,
-        });
-
-        // regular int
-        let test_name = String::from("regular int");
-        let mut times = Vec::with_capacity(trials);
-        for _ in 0..trials {
-            let now = Instant::now();
-            regular::multiply_int(&a_int, &b_int, sizes);
-            times.push(now.elapsed());
-        }
-        perf_blocks.push(PerfBlock {
-            name: test_name,
-            time: times.into_iter().map(|x| x.as_secs_f64()).sum::<f64>() / trials as f64,
-            mae: 0.0,
-        });
-
-        // monte carlo 1152
-        let test_name = String::from("monte 1152");
-        let mut times = Vec::with_capacity(trials);
-        let mut accuracies = Vec::with_capacity(trials);
-        for _ in 0..trials {
-            let now = Instant::now();
-            let c = monte::multiply(&a, &b, sizes, 1152);
-            times.push(now.elapsed());
-            accuracies.push(stats(&a, &c));
-        }
-        perf_blocks.push(PerfBlock {
-            name: test_name,
-            time: times.into_iter().map(|x| x.as_secs_f64()).sum::<f64>() / trials as f64,
-            mae: accuracies.into_iter().sum::<f64>() / trials as f64,
-        });
-
-        // monte carlo 768
-        let test_name = String::from("monte 768");
-        let mut times = Vec::with_capacity(trials);
-        let mut accuracies = Vec::with_capacity(trials);
-        for _ in 0..trials {
-            let now = Instant::now();
-            let c = monte::multiply(&a, &b, sizes, 768);
-            times.push(now.elapsed());
-            accuracies.push(stats(&a, &c));
-        }
-        perf_blocks.push(PerfBlock {
-            name: test_name,
-            time: times.into_iter().map(|x| x.as_secs_f64()).sum::<f64>() / trials as f64,
-            mae: accuracies.into_iter().sum::<f64>() / trials as f64,
-        });
-
-        // monte carlo 384
-        let test_name = String::from("monte 384");
-        let mut times = Vec::with_capacity(trials);
-        let mut accuracies = Vec::with_capacity(trials);
-        for _ in 0..trials {
-            let now = Instant::now();
-            let c = monte::multiply(&a, &b, sizes, 384);
-            times.push(now.elapsed());
-            accuracies.push(stats(&a, &c));
-        }
-        perf_blocks.push(PerfBlock {
-            name: test_name,
-            time: times.into_iter().map(|x| x.as_secs_f64()).sum::<f64>() / trials as f64,
-            mae: accuracies.into_iter().sum::<f64>() / trials as f64,
-        });
-
-        // monte carlo 192
-        let test_name = String::from("monte 192");
-        let mut times = Vec::with_capacity(trials);
-        let mut accuracies = Vec::with_capacity(trials);
-        for _ in 0..trials {
-            let now = Instant::now();
-            let c = monte::multiply(&a, &b, sizes, 192);
-            times.push(now.elapsed());
-            accuracies.push(stats(&a, &c));
-        }
-        perf_blocks.push(PerfBlock {
-            name: test_name,
-            time: times.into_iter().map(|x| x.as_secs_f64()).sum::<f64>() / trials as f64,
-            mae: accuracies.into_iter().sum::<f64>() / trials as f64,
-        });
-
-
-
-        println!("{:?}", perf_blocks);
-
-
-
-
-    }
-
-    //#[test]
-    //fn it_works() {
-    //    let result = add(2, 2);
-    //    assert_eq!(result, 4);
-    //}
-}
+//#[cfg(test)]
+//mod tests {
+//    use common::tools::stats;
+//
+//    use super::*;
+//    use std::time::Instant;
+//
+//
+//    #[derive(Debug, Clone)]
+//    struct PerfBlock {
+//        name: String,
+//        time: f64,
+//        mae: f64,
+//    }    
+//
+//    #[test]
+//    fn sayhi() {
+//        println!("hi");
+//    }
+//
+//    #[test]
+//
+//    //#[test]
+//    //fn it_works() {
+//    //    let result = add(2, 2);
+//    //    assert_eq!(result, 4);
+//    //}
+//}
 
 
---
+++ testing/main.py
@@ -0,0 +1,100 @@
+import numpy as np
+
+a = np.array([
+    [0,  1,  2,  3,  4,  5,  6 ],
+    [7,  8,  9,  10, 11, 12, 13],
+    [14, 15, 16, 17, 18, 19, 20],
+])
+
+b = np.array([
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+])
+
+
+def get_coord(i: int, n: int) -> float:
+    return i / n
+
+def get_index(c: float, n: int) -> int:
+    return round(n * c)
+
+
+#b_cell = (2, 1) #cell we've chosen
+b_cell = (1, 2) #cell we've chosen
+#bx_start = b_cell[0] / b.shape[0] #x
+#bx_end = (b_cell[0] + 1) / b.shape[0]
+#by_start = b_cell[1] / b.shape[1] #y
+#by_end = (b_cell[1] + 1) / b.shape[1]
+# coords
+bx_start = get_coord(b_cell[0], b.shape[0]) #x
+bx_end = get_coord(b_cell[0] + 1, b.shape[0])
+by_start = get_coord(b_cell[1], b.shape[1]) #y
+by_end = get_coord(b_cell[1] + 1, b.shape[1])
+
+print(bx_start)
+print(bx_end)
+print(by_start)
+print(by_end)
+print("-----")
+
+# indices
+ax_start = get_index(bx_start, a.shape[0]) - 1
+ax_end = get_index(bx_end, a.shape[0])
+ay_start = get_index(by_start, a.shape[1]) - 1
+ay_end = get_index(by_end, a.shape[1])
+
+print(f"{ax_start}->{ax_end}, {ay_start}->{ay_end}")
+
+print(f"{bx_start}")
+print(f"{bx_end}")
+print()
+
+print(f"{get_coord(ax_start, a.shape[0])}")
+print(f"{get_coord(ax_end, a.shape[0])}")
+print()
+
+
+print(f"{by_start}")
+print()
+
+print(f"{get_coord(ay_start, a.shape[1])} --")
+print(f"{get_coord(ay_start+1, a.shape[1])} --")
+print(f"{get_coord(ay_start+1, a.shape[1])}")
+print(f"{get_coord(ay_start+2, a.shape[1])}")
+print()
+
+print(f"{by_end}")
+print()
+
+print(f"{get_coord(ay_end-1, a.shape[1])}")
+print(f"{get_coord(ay_end, a.shape[1])}")
+print(f"{get_coord(ay_end, a.shape[1])} --")
+print(f"{get_coord(ay_end+1, a.shape[1])} --")
+print()
+
+
+def get_matched(chosen: tuple[int, int], B_sizes: tuple[int, int], A_sizes: tuple[int, int]) -> tuple[int, int, int, int]:
+    bx_start = get_coord(chosen[0], B_sizes[0]) #x
+    bx_end = get_coord(chosen[0] + 1, B_sizes[0])
+    by_start = get_coord(chosen[1], B_sizes[1]) #y
+    by_end = get_coord(chosen[1] + 1, B_sizes[1])
+
+    ax_start = get_index(bx_start, A_sizes[0]) - 1
+    ax_end = get_index(bx_end, A_sizes[0])
+    ay_start = get_index(by_start, A_sizes[1]) - 1
+    ay_end = get_index(by_end, A_sizes[1])
+
+    return (ax_start, ax_end, ay_start, ay_end)
+
+print(b_cell)
+print(f"{b[b_cell]}")
+results = get_matched(b_cell, b.shape, a.shape)
+print(results)
+#print(f"{a[results[0]]}, {a[results[1]]}, {a[results[2]]}, {a[results[3]]}")
+print(f"{a[results[0], results[2]]}, {a[results[1], results[3]]}")
+
+
+
+
+
+
